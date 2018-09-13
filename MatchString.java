679d848cf8f6fd1cf39b0d618935e0bf7a7c9863
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-09-13 17:33:23 +0200

    Create MatchString.java

---
+++ MatchString.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class MatchString {
+
+    private String[] dictionary = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"};
+
+    public String insertPswd() {
+
+        Scanner in = new Scanner(System.in);
+
+        //faccio inserire la pswd all'utente
+        System.out.print("Inserisci la password: ");
+        String pswd = in.nextLine();
+        System.out.print("Stampo quello che hai inserito: " + pswd);
+
+        //converto la pswd in string array
+        String pswdArr[] = pswd.split("");
+
+        //inizializzo il contatore
+        String matched = "";
+
+        //ciclo la passwd e il dizionario
+        for (int i=0; i<pswd.length(); i++) {
+            for (int y=0; y<dictionary.length; y++) {
+                if (dictionary[y] == pswdArr[i]) {
+                    matched = matched + y;
+                    System.out.println("Questa è la tua password: " + matched);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public String match() {
+
+
+        return null;
+    }
+
+}
